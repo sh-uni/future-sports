6fa7c6ffca11184238e721554f7de2a33b88a2dc
Sophie Holland <soapieh@gmail.com>
2024-06-21 17:06:20 +1000

    updated testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,42 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from './App';
+import { Carousel } from 'react-responsive-carousel';
+
+describe('App sections', () => {
+  test('renders welcome text', () => {
+    render(<App />);
+    expect(screen.getByText(/introduce/)).toBeInTheDocument();
+  });
+  test('renders main text', () => {
+    render(<App />);
+    expect(screen.getByText(/Quidditch/)).toBeInTheDocument();
+  });
+  test('renders news', () => {
+    render(<App />);
+    expect(screen.getByText(/Latest News/)).toBeInTheDocument();
+  });
+  test('renders events', () => {
+    render(<App />);
+    expect(screen.getByText(/Upcoming Events/)).toBeInTheDocument();
+  });
+  test('renders links', () => {
+    render(<App />);
+    expect(screen.getByText(/Links/)).toBeInTheDocument();
+  });
+  
+});
+
+describe('App components', () => {
+  test('renders main img', () => {
+    render(<App />); 
+    expect(screen.getByAltText(/Quadball Team Break/)).toBeInTheDocument();
+  });
+
+  test('renders search component', () => {
+    render(<App />); 
+    expect(screen.getByPlaceholderText("search...")).toBeInTheDocument();
+  });
+
 });
